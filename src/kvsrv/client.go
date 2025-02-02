772e399d3b005d2ddde75281b590eb058e81928d
nitishsharma2825 <sharnit2825@gmail.com>
2025-02-02 10:24:14 +0000

    lab2A pass

--- src/kvsrv/client.go
+++ src/kvsrv/client.go
@@ -1,9 +1,12 @@
 package kvsrv
 
-import "6.5840/labrpc"
-import "crypto/rand"
-import "math/big"
+import (
+	"crypto/rand"
+	"log"
+	"math/big"
 
+	"6.5840/labrpc"
+)
 
 type Clerk struct {
 	server *labrpc.ClientEnd
@@ -35,9 +38,23 @@ func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) string {
-
 	// You will have to modify this function.
-	return ""
+
+	args := GetArgs{
+		Key: key,
+	}
+
+	reply := GetReply{
+		Value: "",
+	}
+
+	ok := ck.server.Call("KVServer.Get", &args, &reply)
+	if !ok {
+		log.Fatal("RPC failed")
+		return ""
+	}
+
+	return reply.Value
 }
 
 // shared by Put and Append.
@@ -50,7 +67,23 @@ func (ck *Clerk) Get(key string) string {
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) string {
 	// You will have to modify this function.
-	return ""
+
+	args := PutAppendArgs{
+		Key:   key,
+		Value: value,
+	}
+
+	reply := PutAppendReply{
+		Value: "",
+	}
+
+	ok := ck.server.Call("KVServer."+op, &args, &reply)
+	if !ok {
+		log.Fatal("RPC failed")
+		return ""
+	}
+
+	return reply.Value
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -14,30 +14,40 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
+	db map[string]string
 }
 
-
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	reply.Value = kv.db[args.Key]
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	kv.db[args.Key] = args.Value
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	oldval := kv.db[args.Key]
+	newval := oldval + args.Value
+	kv.db[args.Key] = newval
+	reply.Value = oldval
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
 
 	// You may need initialization code here.
+	kv.db = make(map[string]string)
+	kv.mu = sync.Mutex{}
 
 	return kv
 }
